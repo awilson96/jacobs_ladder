7e210c944894f0cd5035dc692e44e0486ca75dd1
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-06-23 18:07:23 -0400

    Added createInterval, + and - overloads

--- hello_world/Intervals.h
+++ hello_world/Intervals.h
@@ -8,7 +8,7 @@
 //project includes
 #include "Notes.h"
 
-enum class Intervals : int
+enum class Interval : int
 {
     semitone        = 1,
     wholeStep       = 2,
@@ -21,12 +21,54 @@ enum class Intervals : int
     sixth           = 9,
     minorSeventh    = 10,
     majorSeventh    = 11,
-    octave          = 12
+    octave          = 12,
+    unknown         = 13
 };
 
-bool operator==(Intervals lhs, Note rhs) { return static_cast<int>(lhs) == static_cast<int>(rhs); }
-bool operator==(Note lhs, Intervals rhs) { return static_cast<int>(lhs) == static_cast<int>(rhs); }
-bool operator==(Intervals lhs, int rhs) { return static_cast<int>(lhs) == rhs; }
-bool operator==(int lhs, Intervals rhs) { lhs == static_cast<int>(rhs); }
+Interval createInterval(int interval)
+{
+    switch(interval)
+    {
+        case 1:
+            return Interval::semitone;
+        case 2:
+            return Interval::wholeStep;
+        case 3:
+            return Interval::minorThird;
+        case 4:
+            return Interval::majorThird;
+        case 5:
+            return Interval::fourth;
+        case 6:
+            return Interval::tritone;
+        case 7:
+            return Interval::fifth;
+        case 8:
+            return Interval::minorSixth;
+        case 9:
+            return Interval::sixth;
+        case 10:
+            return Interval::minorSeventh;
+        case 11:
+            return Interval::majorSeventh;
+        case 12:
+            return Interval::octave;
+        default:
+            return Interval::unknown;
+    }
+}
+
+Interval operator+(Interval lhs, Interval rhs) 
+    { return static_cast<Interval>(static_cast<int>(lhs) + static_cast<int>(rhs)); }
+
+Interval operator-(Interval lhs, Interval rhs) 
+    { return static_cast<Interval>(static_cast<int>(lhs) - static_cast<int>(rhs)); }
+
+bool operator==(Interval lhs, Note rhs) { return static_cast<int>(lhs) == static_cast<int>(rhs); }
+bool operator==(Note lhs, Interval rhs) { return static_cast<int>(lhs) == static_cast<int>(rhs); }
+bool operator==(Interval lhs, int rhs) { return static_cast<int>(lhs) == rhs; }
+bool operator==(int lhs, Interval rhs) { lhs == static_cast<int>(rhs); }
+
+
 
 #endif // INTERVALS_H
