04db733b95905dcf51c0bee1c0bbe95ee6c563f1
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-06-23 23:47:11 -0400

    Further restructuring

---
+++ src/Triads.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#include "Notes.h"
+#include "Intervals.h"
+
+
+namespace Fundamentals 
+{
+    std::string determineChord(Note bass, Note tenor, Note treble)
+    {
+        // Get the intervals between all three notes
+        Interval bassTenor = tenor - bass;
+        Interval tenorTreble = treble - tenor;
+        Interval bassTreble = treble - bass;
+    }
+}
---
+++ src/Triads.h
@@ -0,0 +1,21 @@
+#ifndef TRIADS_H
+#define TRIADS_H
+
+// file includes
+#include <iostream>
+#include <string>
+
+//project includes
+#include "Notes.h"
+#include "Intervals.h"
+
+namespace Fundamentals
+{
+    std::string determineChord(Note bass, Note tenor, Note treble);
+
+}
+
+
+
+
+#endif // TRIADS_H
