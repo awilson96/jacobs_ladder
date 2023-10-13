cad80aed4e22722f590948c6ce116950dbfd0406
Alex Wilson <awilson2244@gmail.com>
2023-10-12 21:56:03 -0400

    Deleted old stuff

--- docker-startup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Build the Docker image
-echo Building the Docker image...
-docker build -t cpp-dev .
-
-REM Run the Docker container
-echo Running the Docker container...
-docker run -it cpp-dev
