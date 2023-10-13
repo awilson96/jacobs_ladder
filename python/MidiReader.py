6d44e2159d447852de5847a4b6ad69c38422ed52
Alex Wilson <awilson2244@gmail.com>
2023-10-12 23:08:24 -0400

    Made a note_on heap to record the state of notes which are currently on

--- python/MidiReader.py
+++ python/MidiReader.py
@@ -1,4 +1,7 @@
 import rtmidi
+import time
+import numpy as np
+import heapq
 
 class MidiController:
     def __init__(self, port_name="jacobs_ladder 2"):
@@ -6,6 +9,9 @@ class MidiController:
         self.available_ports = self.midi_in.get_ports()
         print(self.available_ports)
         self.port_name = port_name
+        # Set NumPy print options and display values to 3 decimal places
+        np.set_printoptions(suppress=True, precision=3)
+        self.note_heap = []  # Heap to store active notes (message format: ([status, note, velocity], timestamp))
 
     def open_port(self):
         if self.port_name in self.available_ports:
@@ -16,18 +22,30 @@ class MidiController:
             print(f"MIDI input port '{self.port_name}' not found.")
             exit()
 
-    def on_midi_message(self, message, time_stamp):
+    def on_midi_message(self, message, timestamp):
         print(message)
+        midi_event, _ = message
+        status, note, velocity = midi_event
+        if status == 148:  # Note On event
+            heapq.heappush(self.note_heap, [note, velocity, timestamp])
+            print(f"{self.note_heap}")
+        elif status == 132:  # Note Off event
+            # Remove the note from the heap if it is present
+            self.note_heap = [note_info for note_info in self.note_heap if note_info[0] != note]
+            print(f"{self.note_heap}")
 
     def set_midi_callback(self):
         self.midi_in.set_callback(self.on_midi_message)
 
     def start_listening(self):
         try:
-            self.set_midi_callback()
             print("Listening for MIDI messages. Press Ctrl+C to exit.")
             while True:
-                pass
+                message = self.midi_in.get_message()
+                if message:
+                    _, dt = message
+                    print("Received MIDI message:", message, "Timestamp:", dt)
+                time.sleep(0.001)  # Add a small delay to control the polling rate
         except KeyboardInterrupt:
             print("Exiting...")
         finally:
@@ -38,7 +56,8 @@ class MidiController:
 
 def main():
     midi_controller = MidiController()
-    midi_controller.open_port()  # Opens the specified MIDI port
+    midi_controller.open_port()
+    midi_controller.set_midi_callback()
     midi_controller.start_listening()
 
 if __name__ == "__main__":
