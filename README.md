24957f8f2defd8d6f68309c5d631bd839496f7d4
Alex Wilson <awilson2244@gmail.com>
2023-10-13 19:47:01 -0400

    Reformatted and added some TODOs in the readme

--- README.md
+++ README.md
@@ -10,8 +10,20 @@ To get started with running MidiReader.py do the following:
 1. Download MIDI-OX and loopMIDI
 2. Setup two virtual ports in loopMIDI. This is done by creating a name for the port and pressing the plus sign.  Name the first port jacobs_ladder and the second port software_instrument. Note that if you close out of the loopMIDI program the ports stay up.  To stop the connection you must click on the port in loopMIDI and then click the minus sign.
 3. Now open up MIDI-OX and select Options > Midi Devices...
-4. A new window will open with all of the devices you have available for MIDI input and output. For Midi Input select your keyboard, and for Midi output select the two ports you created in step 2.
+4. A new window will open with all of the devices you have available for MIDI input and output. For Midi Input select your keyboard, and for Midi output select the ports you created in step 2 called jacobs_ladder
 5. After selecting OK you can test the connection by pressing keys on your keyboard. You should see the raw data coming through on the MIDI-OX - [Monitor - Output]
 6. Now navigate to MidiReader.py and run the script. You may need to adjust the port in the initializer so that it matches the port you created in step 2. If you don't know how to do this then uncomment the print statement in the class constructor to see a list of all the ports. If `jacobs_ladder` is the third port in the list, then you would set port_name='jacobs_ladder 3' in the initializer. 
 7. After running MidiReader.py, ensure that you are getting MIDI data printed to the console when you press keys on the keyboard
-8. Now open up your software instrument and connect it to the port called software_instrument. At this stage you should see that your able to hear the output of the MIDI data in your software instrument.
+8. Now open up your software instrument and connect it to the port called software_instrument. At this stage you should see that your able to hear the output of the MIDI data in your software instrument.  Note that your software instrument should play nothing if the script is not running.
+
+# Next Steps
+The current progress is that I got each note to be sent out on different channels which will be useful for just intonation
+There are a few things that need to be done:
+1. The code should be optimized:
+  - The self.available_channels_heap needs only one param, either the note_on or note off param not both since one can be derived from the other
+  - The variable names should be renamed so that they do not get confused, in one loop there is n note and notes all in the same place making it unclear which is which
+2. I need to think about what happens once I add a sustain pedal into the mix, and how that will change things. Specifically, what happens when the number of channels runs out when holding the sustain pedal? Does the ocatave optimization mostly save us from this pitfall or is there a need to setup a second output port and route two outut ports into the software instrument
+3. What is the most effecient way to track the state of things. The status byte may need to go somewhere else in the note_heap since the heap is sorted according to the first parameter that is put into the heap. The note is the main data that will be accessed therefore having it already sorted will save a lot of time and effort if we don't need to rely on sorting given the heap state. 
+4. I need to build a pitch shift function which takes as params the status byte (from which the channel can be extracted) and the number of cents need to be shifted up or down (minus for down + for positive) It may be more effecient to store the self.available_channels_heap as hex instead of decimal so that there is no need of conversion. The only problem then is how do I add and subtract numbers in hex? May need to see if thisis worth it. 
+5. I need a class variable called self.fulcrum which will act as the place from which all other notes will be tuned from. However fulcrum may not be a single note it may need to be a list of the most recently played notes.  Most recently will need to be determined using dt, but also may need to be based on which notes are still active.  This choice will be very difficult and will proabably need to be handled on a case by case basis.
+6. It may also be useful to create a class variable called self.key which records the key of the music we are currently playing in.  This variable will be set by a method called set_key() and will also likely need some history to determine what key we are actually in. 
--- python/MidiReader.py
+++ python/MidiReader.py
@@ -7,9 +7,10 @@ __author__ = "Alex Wilson"
 __copyright__ = "Copyright (c) 2023 Jacob's Ladder"
 __date__ = "October 12th 2023 (creation)"
 
+
 class MidiController:
     """
-    This is a Midi Keyboard interface which allows for the manipulation of real time Midi data 
+    This is a Midi Keyboard interface which allows for the manipulation of real time Midi data
     before sending that data back out through a virtual port to another program such as a software instrument
     """
 
@@ -32,16 +33,29 @@ class MidiController:
 
         # Heap to store active notes (message format: ([status, note, velocity], timestamp))
         self.note_heap = []
-        self.available_channels_heap = [[144, 128], [145, 129], [146, 130], [147, 131], [148, 132], [149, 133], [150, 134], [151, 135], 
-                                        [152, 136], [153, 137], [154, 138], [155, 139], [156, 140], [157, 141], [158, 142], [159, 143]]
+        self.available_channels_heap = [
+            [144, 128],
+            [145, 129],
+            [146, 130],
+            [147, 131],
+            [148, 132],
+            [149, 133],
+            [150, 134],
+            [151, 135],
+            [152, 136],
+            [153, 137],
+            [154, 138],
+            [155, 139],
+            [156, 140],
+            [157, 141],
+            [158, 142],
+            [159, 143],
+        ]
         heapq.heapify(self.available_channels_heap)
         self.note_off = {}
-        
-        
 
     def open_ports(self):
-        """This function used the ports extracted from the initializer to open Midi input and output ports
-        """
+        """This function used the ports extracted from the initializer to open Midi input and output ports"""
         if self.input_port in self.available_input_ports:
             port_number = self.available_input_ports.index(self.input_port)
             self.midi_in.open_port(port_number)
@@ -77,28 +91,30 @@ class MidiController:
                 new_status, status_off = self.available_channels_heap.pop()
                 heapq.heappush(self.note_heap, [new_status, note, velocity, dt])
                 self.note_off[note] = status_off
-                
+
             self.midi_out.send_message([new_status, note, velocity])
-            
+
             print(f"{self.note_heap}")
 
         # Note Off event
-        elif status in range (128, 144):
+        elif status in range(128, 144):
             new_status = self.note_off[note]
             if [new_status + 16, new_status] not in self.available_channels_heap:
-                heapq.heappush(self.available_channels_heap, [new_status + 16, new_status])
+                heapq.heappush(
+                    self.available_channels_heap, [new_status + 16, new_status]
+                )
             # Remove the note from the heap if it is present
             self.midi_out.send_message([new_status, note, velocity])
             self.note_heap = [
                 note_info for note_info in self.note_heap if note_info[1] != note
             ]
-            
+
     def determineOctave(self, note):
-        """Determines if the current note is an octave of any currently active note. 
+        """Determines if the current note is an octave of any currently active note.
         If so then it is assigned the channel corresponding to the note in note_heap which is its octave.
 
         Args:
-            note (int): the active Midi note that is being considered 
+            note (int): the active Midi note that is being considered
 
         Returns:
             int: status, the status which encapsulates the Midi channel information. Notes which are octaves of eachother can be on the same channel
@@ -108,18 +124,16 @@ class MidiController:
         status = list(map(get_status, self.note_heap))
         notes = list(map(get_notes, self.note_heap))
         for n in notes:
-            if abs(note-n) == 12:
+            if abs(note - n) == 12:
                 return status[notes.index(n)]
         return None
 
     def set_midi_callback(self):
-        """This function filters the output to the console based on the on_midi_message function
-        """
+        """This function filters the output to the console based on the on_midi_message function"""
         self.midi_in.set_callback(self.on_midi_message)
 
     def start_listening(self):
-        """This is the main loop where execution takes place. The listener waits for Midi messages and acts accordingly based on supporting functions
-        """
+        """This is the main loop where execution takes place. The listener waits for Midi messages and acts accordingly based on supporting functions"""
         try:
             print("Listening for MIDI messages. Press Ctrl+C to exit.")
             while True:
@@ -128,21 +142,19 @@ class MidiController:
                     _, dt = message
                     print("Received MIDI message:", message, "Timestamp:", dt)
                 # Add a small delay to control the polling rate
-                time.sleep(0.001)  
+                time.sleep(0.001)
         except KeyboardInterrupt:
             print("Exiting...")
         finally:
             self.close_port()
 
     def close_port(self):
-        """Close the ports at the end of execution
-        """
+        """Close the ports at the end of execution"""
         self.midi_in.close_port()
         self.midi_out.close_port()
 
     def just_intonation(self):
-        """Retune the keyboard to match just intonation
-        """
+        """Retune the keyboard to match just intonation"""
         if len(self.note_heap) == 3:
             status1, note1, _, _ = self.note_heap[0]
             status2, note2, _, _ = self.note_heap[1]
@@ -154,8 +166,7 @@ class MidiController:
 
 
 def main():
-    """Instantiates the class, opens ports, sets the callback for filtered Midi output, and listens for events
-    """
+    """Instantiates the class, opens ports, sets the callback for filtered Midi output, and listens for events"""
     midi_controller = MidiController()
     midi_controller.open_ports()
     midi_controller.set_midi_callback()
