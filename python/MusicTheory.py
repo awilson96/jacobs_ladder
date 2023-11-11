b4d8516bfb3884c37f84315bc5c2c498a8d914eb
Alex Wilson <awilson2244@gmail.com>
2023-11-11 14:56:41 -0500

    Init commit MusicTheory

---
+++ python/MusicTheory.py
@@ -0,0 +1,18 @@
+import logging
+
+__author__ = "Alex Wilson"
+__copyright__ = "Copyright (c) 2023 Jacob's Ladder"
+__date__ = "November 11th 2023 (creation)"
+
+logging.basicConfig(
+    filename="../logs/MidiManager.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
+class MusicTheory:
+    def __init__(self):
+        pass
+    
+    
