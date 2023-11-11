bbaedea16ddc6e82a84278149e0486f81be12d66
Alex Wilson <awilson2244@gmail.com>
2023-11-11 16:03:41 -0500

    Added initial implementation to get_diad

--- python/MusicTheory.py
+++ python/MusicTheory.py
@@ -46,3 +46,67 @@ class MusicTheory:
                 intervals.append(notes[idx+1] - notes[idx])
         
         return intervals
+    
+    def get_diad(self, intervals: list[int]):
+        """
+        Get the stringified name of a diad interval, used to make tuning decisions or displaying to the terminal
+
+        Args:
+            intervals (int): A list of integer interval used to determine intervallic relationships between notes
+
+        Returns:
+            list[string]: list of intervallic relationships expressed as strings
+        """
+        diads: list[str] = []
+        
+        for interval in intervals:
+            match interval:
+                case 1:
+                    diads.append("Minor 2")
+                case 2:
+                    diads.append("Major 2")
+                case 3:
+                    diads.append("Minor 3")
+                case 4:
+                    diads.append("Major 3")
+                case 5:
+                    diads.append("Perfect 4")
+                case 6:
+                    diads.append("Minor 5")
+                case 7: 
+                    diads.append("Perfect 5")
+                case 8:
+                    diads.append("Minor 6")
+                case 9:
+                    diads.append("Major 6")
+                case 10:
+                    diads.append("Minor 7")
+                case 11:
+                    diads.append("Major 7")
+                case 12:
+                    diads.append("Octave 8")
+                case 13:
+                    diads.append("Minor 9")
+                case 14:
+                    diads.append("Major 9")
+                case 15:
+                    diads.append("Minor 10")
+                case 16:
+                    diads.append("Major 10")
+                case 17:
+                    diads.append("Major 11")
+                case 18:
+                    diads.append("Sharp 11")
+                case 19:
+                    diads.append("Major 12")
+                case 20:
+                    diads.append("Minor 13")
+                case 21:
+                    diads.append("Major 13")
+                case 22:
+                    diads.append("Minor 14")
+                case 23:
+                    diads.append("Major 14")
+                case 24:
+                    diads.append("Octave 15")
+        return diads
