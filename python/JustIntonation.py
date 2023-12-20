5b79bad5df574078887678da0fb76275aeb8729e
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-12-20 14:04:25 -0500

    Added a new enum Pitch to abstract away the analog tuning from ET to JI

--- python/JustIntonation.py
+++ python/JustIntonation.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 
 __author__ = "Alex Wilson"
 __copyright__ = "Copyright (c) 2023 Jacob's Ladder"
@@ -59,13 +60,12 @@ class JustIntonation:
 
         if chord is not None:
             if "Major Triad" in chord:
-                # TODO: Instead of using raw ints (7632 and 8272) calculate these values in another function instead or store cached tuning values
                 action1 = (
-                    self.get_pitch_bend_message(sorted_message_heap[1], 7632),
+                    self.get_pitch_bend_message(sorted_message_heap[1], Pitch.major_third),
                     instance_indices[1],
                 )
                 action2 = (
-                    self.get_pitch_bend_message(sorted_message_heap[2], 8272),
+                    self.get_pitch_bend_message(sorted_message_heap[2], Pitch.perfect_fifth),
                     instance_indices[2],
                 )
                 # TODO: keep state of detuned instances to be used by the recenter frequency function
@@ -85,3 +85,34 @@ class JustIntonation:
             instance_index (int): the instance index of the note which has received the note off message
         """
         pass
+
+
+class Pitch(Enum):
+    """The pitchwheel enum is meant to abstract away the just intervals after thay have been converted into analog
+    values.  The analog values represent how far away from the equally tempered value to bend the pitch in order to
+    make it a just interval
+
+    Args:
+        Enum (int): The analog pitch value between 0 and 16383 where 8192 represents no tuning, 0 down a whole step and
+        16383 represents going up a whole step.
+    """
+    minor_second = 8712     # 16/15
+    major_second = 8352     # 9/8
+    minor_third = 8860      # 6/5
+    major_third = 7619      # 5/4
+    perfect_fourth = 8110   # 4/3
+    tritone = 7791          # 45/32
+    perfect_fifth = 8270    # 3/2
+    minor_sixth = 8753      # 8/5
+    major_sixth = 7553      # 5/3
+    minor_seventh = 8032    # 9/5
+    major_seventh = 7713    # 15/8
+    octave = 8192           # 2/1
+    # minor_ninth = 1
+    # major_ninth = 1
+    # minor_tenth = 1
+    # major_tenth = 1
+    # major_eleventh = 1
+    # sharp_eleventh = 1
+    # minor_thirtenth = 1
+    # major_thirtenth = 1
