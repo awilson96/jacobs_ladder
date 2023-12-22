bdcff9f66a2513e13a7ab68633f679c4e198dded
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-12-22 13:49:09 -0500

    Refactored MusicTheory after moving InOutQueue, Major Scales, and Scale dataclass

--- python/MusicTheory.py
+++ python/MusicTheory.py
@@ -1,5 +1,7 @@
 import logging
 from dataclasses import dataclass
+from MajorScales import get_major_scales
+from Utilities import InOutQueue
 
 __author__ = "Alex Wilson"
 __copyright__ = "Copyright (c) 2023 Jacob's Ladder"
@@ -10,45 +12,6 @@ logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-
-
-class InOutQueue:
-    def __init__(self, size):
-        self.queue = []
-        self.size = size
-
-    def enqueue(self, elem):
-        self.queue.append(elem)
-        if len(self.queue) > self.size:
-            self.queue.pop(0)  # Remove the oldest element at index 0
-
-    def get_queue(self):
-        return self.queue
-
-
-@dataclass
-class Scale:
-    name: str
-    notes: list[str]   
-    
-# Major/Minor scales
-C_Major = Scale("C Ionian", ["C", "D", "E", "F", "G", "A", "B"])
-Db_Major = Scale("Db Ionian", ["D\u266d", "E\u266d", "F", "G\u266d", "A\u266d", "B\u266d", "C"])
-D_Major = Scale("D Ionian", ["D", "E", "G\u266d", "G", "A", "B", "D\u266d"])
-Eb_Major = Scale("Eb Ionian", ["E\u266d", "F", "G", "A\u266d", "B\u266d", "C", "D"])
-E_Major = Scale("E Ionian", ["E", "G\u266d", "A\u266d", "A", "B", "D\u266d", "E\u266d"])
-F_Major = Scale("F Ionian", ["F", "G", "A", "B\u266d", "C", "D", "E"])
-Gb_Major = Scale("Gb Ionian", ["G\u266d", "A\u266d", "B\u266d", "B", "D\u266d", "E\u266d", "F"])
-G_Major = Scale("G Ionian", ["G", "A", "B", "C", "D", "E", "G\u266d"])
-Ab_Major = Scale("Ab Ionian", ["A\u266d", "B\u266d", "C", "D\u266d", "E\u266d", "F", "G"])
-A_Major = Scale("A Ionian", ["A", "B", "D\u266d", "D", "E", "G\u266d", "A\u266d"])
-Bb_Major = Scale("Bb Ionian", ["B\u266d", "C", "D", "E\u266d", "F", "G", "A"])
-B_Major = Scale("B Ionian", ["B", "D\u266d", "E\u266d", "E", "G\u266d", "A\u266d", "B\u266d"])
-
-# TODO: Add Harmonic Minor Scales
-# TODO: Add Harmonic Major Scales
-# TODO: Add Melodic Minor Scales
-# TODO: Determine where these scales should live since they are all just dataclasses
 
 class MusicTheory:
     def __init__(self):
@@ -61,7 +24,7 @@ class MusicTheory:
                          93: "A", 94: "B\u266d", 95: "B", 96: "C", 97: "D\u266d", 98: "D", 99: "E\u266d", 100: "E", 101: "F", 102: "G\u266d", 103: "G", 104: "A\u266d",
                          105: "A", 106: "B\u266d", 107: "B", 108: "C"}
         
-        self.major_scales = [C_Major, Db_Major, D_Major, Eb_Major, E_Major, F_Major, Gb_Major, G_Major, Ab_Major, A_Major, Bb_Major, B_Major]
+        self.major_scales = get_major_scales()
 
         # History of at most the last 5 lists of candidate keys used to determine the key uniquely at a given point in time
         # TODO: Determine the optimum lookback period (more than 5, less than 5?)
