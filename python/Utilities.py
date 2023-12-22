880a7d57c5246994e613fedad8466243f227791f
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-12-22 13:48:29 -0500

    Moved InOutQueue and Scale dataclass to a Utilities file

---
+++ python/Utilities.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+class InOutQueue:
+    def __init__(self, size):
+        self.queue = []
+        self.size = size
+
+    def enqueue(self, elem):
+        self.queue.append(elem)
+        if len(self.queue) > self.size:
+            self.queue.pop(0)  # Remove the oldest element at index 0
+
+    def get_queue(self):
+        return self.queue
+    
+
+@dataclass
+class Scale:
+    name: str
+    notes: list[str]
