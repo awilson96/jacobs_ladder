8f234538d5ce514a6be954e2730c7c865d46b078
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-12-22 15:13:52 -0500

    Moved Pitch Enum to a separate file in utilities

--- python/music/JustIntonation.py
+++ python/music/JustIntonation.py
@@ -1,5 +1,6 @@
 import logging
-from enum import Enum
+
+from ..utilities.Enums import Pitch
 
 __author__ = "Alex Wilson"
 __copyright__ = "Copyright (c) 2023 Jacob's Ladder"
@@ -85,34 +86,3 @@ class JustIntonation:
             instance_index (int): the instance index of the note which has received the note off message
         """
         pass
-
-
-class Pitch(Enum):
-    """The pitchwheel enum is meant to abstract away the just intervals after thay have been converted into analog
-    values.  The analog values represent how far away from the equally tempered value to bend the pitch in order to
-    make it a just interval
-
-    Args:
-        Enum (int): The analog pitch value between 0 and 16383 where 8192 represents no tuning, 0 down a whole step and
-        16383 represents going up a whole step.
-    """
-    minor_second = 8712     # 16/15
-    major_second = 8352     # 9/8
-    minor_third = 8860      # 6/5
-    major_third = 7619      # 5/4
-    perfect_fourth = 8110   # 4/3
-    tritone = 7791          # 45/32
-    perfect_fifth = 8270    # 3/2
-    minor_sixth = 8753      # 8/5
-    major_sixth = 7553      # 5/3
-    minor_seventh = 8032    # 9/5
-    major_seventh = 7713    # 15/8
-    octave = 8192           # 2/1
-    # minor_ninth = 1
-    # major_ninth = 1
-    # minor_tenth = 1
-    # major_tenth = 1
-    # major_eleventh = 1
-    # sharp_eleventh = 1
-    # minor_thirtenth = 1
-    # major_thirtenth = 1
