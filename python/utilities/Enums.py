56b361f6305ee578fb0e3a44ebb2728acb2e7692
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-12-23 19:19:09 -0500

    Added pitch up and pitch down options in Enums.py

--- python/utilities/Enums.py
+++ python/utilities/Enums.py
@@ -10,23 +10,26 @@ class Pitch(Enum):
         Enum (int): The analog pitch value between 0 and 16383 where 8192 represents no tuning, 0 down a whole step and
         16383 represents going up a whole step.
     """
-    minor_second = 8712     # 16/15
-    major_second = 8352     # 9/8
-    minor_third = 8860      # 6/5
-    major_third = 7619      # 5/4
-    perfect_fourth = 8110   # 4/3
-    tritone = 7791          # 45/32
-    perfect_fifth = 8270    # 3/2
-    minor_sixth = 8753      # 8/5
-    major_sixth = 7553      # 5/3
-    minor_seventh = 8032    # 9/5
-    major_seventh = 7713    # 15/8
-    octave = 8192           # 2/1
-    # minor_ninth = 1
-    # major_ninth = 1
-    # minor_tenth = 1
-    # major_tenth = 1
-    # major_eleventh = 1
-    # sharp_eleventh = 1
-    # minor_thirtenth = 1
-    # major_thirtenth = 1
+    minor_second_up = 8712     # 16/15
+    major_second_up = 8352     # 9/8
+    minor_third_up = 8860      # 6/5
+    major_third_up = 7619      # 5/4
+    perfect_fourth_up = 8110   # 4/3
+    tritone_up = 7791          # 45/32
+    perfect_fifth_up = 8270    # 3/2
+    minor_sixth_up = 8753      # 8/5
+    major_sixth_up = 7553      # 5/3
+    minor_seventh_up = 8032    # 9/5
+    major_seventh_up = 7713    # 15/8
+    minor_second_down = 8712     # 15/16
+    major_second_down = 8352     # 8/9
+    minor_third_down = 8860      # 5/6
+    major_third_down = 7619      # 4/5
+    perfect_fourth_down = 8110   # 3/4
+    tritone_down = 7791          # 32/45
+    perfect_fifth_down = 8270    # 2/3
+    minor_sixth_down = 8753      # 5/8
+    major_sixth_down = 7553      # 3/5
+    minor_seventh_down = 8032    # 5/9
+    major_seventh_down = 7713    # 8/15
+    octave = 8192                # 2/1 | 1/2
