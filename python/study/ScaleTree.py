0c52c389efa3fd4d41c4ed3d1ef5add798703139
Alex Wilson <awilson2244@gmail.com>
2024-01-30 17:38:44 -0500

    Created new study module for studying obscure music theory

---
+++ python/study/ScaleTree.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import itertools
+
+
+class ScaleTree:
+    """Create 
+    """
+    
+    def __init__(self):
+        self.num_octave_divisions = 12
+        
+    def create_all_n_note_scales(self, degree: int):
+        pass
+    
+
+if __name__ == "__main__":
+    
+    st = ScaleTree()
+
+
