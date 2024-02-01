033c4c480c6b20f20942be37a0648452cf4fa9e3
Alex Wilson <awilson2244@gmail.com>
2024-02-01 00:31:30 -0500

    ScaleClassifier init commit

---
+++ python/study/ScaleClassifier.py
@@ -0,0 +1,5 @@
+from ..utilities.DataClasses import Scale
+from .ScaleTree import ScaleTree
+
+# TODO: Read in dfs to classify scales according to mode.
+# TODO: It may make sense to make an optional argument to the Scale dataclass which is called mode to further classify the scale
