0664aa0975177ec075ae981bc29c767d8847344c
Alex Wilson <awilson2244@gmail.com>
2024-03-30 23:43:32 -0400

    Added boiler plate if statements for handling triads

--- python/music/JustIntonation.py
+++ python/music/JustIntonation.py
@@ -1,6 +1,7 @@
 import logging
 
 from ..utilities.Enums import Pitch
+from .MusicTheory import get_intervals
 
 __author__ = "Alex Wilson"
 __copyright__ = "Copyright (c) 2023 Jacob's Ladder"
@@ -44,7 +45,7 @@ class JustIntonation:
         return pitch_bend_message
 
     def pitch_adjust_chord(self, message_heap: list[list], chord=None):
-        """Ajust the pitch of individual notes within a given chord
+        """Adjust the pitch of individual notes within a given chord
         If the chord is unknown then it will be tuned using intervals instead
 
         Args:
@@ -58,25 +59,296 @@ class JustIntonation:
         sorted_message_heap = sorted(message_heap, key=lambda x: x[0])
         notes = [note[0] for note in sorted_message_heap]
         instance_indices = [indices[1] for indices in sorted_message_heap]
+        intervals = get_intervals(notes=notes)
 
-        # if chord is not None:
-        #     if "Major Triad" in chord:
-        #         action1 = (
-        #             self.get_pitch_bend_message(sorted_message_heap[1], Pitch.major_third.value),
-        #             instance_indices[1],
-        #         )
-        #         action2 = (
-        #             self.get_pitch_bend_message(sorted_message_heap[2], Pitch.perfect_fifth.value),
-        #             instance_indices[2],
-        #         )
-        #         # TODO: keep state of detuned instances to be used by the recenter frequency function
-        #         # self.detuned_instances.append(instance_indices[1])
-        #         # self.detuned_instances.append(instance_indices[2])
-        #         # self.detuned_instances = list(set(self.detuned_instances))
-
-        #         return [action1, action2]
-        # else:
-        #     pass
+        if "major_triad" in chord:
+            # Major (C, E, G) or (C, G, E)
+            pass
+        elif "major_triad_1st_inv" in chord:
+            # Major 1st Inversion (E, G, C)
+            pass
+        elif "major_triad_1st_inv_var" in chord:
+            # Major 1st Inversion (E, C, G)
+            pass
+        elif "major_triad_2nd_inv" in chord:
+            # Major 2nd Inversion (G, C, E)
+            pass
+        elif "major_triad_2nd_inv_var" in chord:
+            # Major 2nd Inversion (G, E, C)
+            pass
+        elif "minor_triad" in chord:
+            # Minor (C, Eb, G)
+            pass
+        elif "minor_triad_1st_inv" in chord:
+            # Minor 1st Inversion (Eb, G, C)
+            pass
+        elif "minor_triad_1st_inv_var" in chord:
+            # Minor 1st Inversion (Eb, C, G)
+            pass
+        elif "minor_triad_2nd_inv" in chord:
+            # Minor 2nd Inversion (G, C, Eb)
+            pass
+        elif "minor_triad_2nd_inv_var" in chord:
+            # Minor 2nd Inversion (G, Eb, C)
+            pass
+        elif "add2" in chord:
+            # Add 2 chord
+            pass
+        elif "add2_1st_inv" in chord:
+            # Add 2 chord 1st inversion
+            pass
+        elif "add2_2nd_inv" in chord:
+            # Add 2 chord 2nd inversion
+            pass
+        elif "diminished" in chord:
+            # Diminished (C, Eb, Gb) or (C, Gb, Eb)
+            pass
+        elif "augmented" in chord:
+            # Augmented (C, E, Ab) or (C, Ab, E)
+            pass
+        elif "eleven_sus4" in chord:
+            # Quartal chord (stacking fourths)
+            pass
+        elif "sus2" in chord:
+            # Suspended 2
+            pass
+        elif "nine_chord" in chord:
+            # 9 chord with no third
+            pass
+        elif "sus4" in chord:
+            # Suspended 4
+            pass
+        elif "eleven_chord" in chord:
+            # 11 chord with no third
+            pass
+        elif "dominant_no_third" in chord:
+            # Dominant with no 3rd
+            pass
+        elif "dominant_no_third_1st_inv" in chord:
+            # Dominant with no 3rd 1st Inversion
+            pass
+        elif "dominant_no_third_2nd_inv" in chord:
+            # Dominant with no 3rd 2nd Inversion
+            pass
+        elif "dominant_no_fifth" in chord:
+            # Dominant with no 5th
+            pass
+        elif "dominant_no_fifth_1st_inv" in chord:
+            # Dominant with no 5th 1st Inversion
+            pass
+        elif "dominant_no_fifth_2nd_inv" in chord:
+            # Dominant with no 5th 2nd Inversion
+            pass
+        elif "min6" in chord:
+            # Minor 6 (Diminished 1st Inversion)
+            pass
+        elif "min6_1st_inv" in chord:
+            # Minor 6 1st inv (Diminished 2nd Inversion)
+            pass
+        elif "maj7_no_third" in chord:
+            # Major 7 no 3rd
+            pass
+        elif "maj7_no_third_1st_inv" in chord:
+            # Major 7 no 3rd 1st Inversion
+            pass
+        elif "maj7_no_third_2nd_inv" in chord:
+            # Major 7 no 3rd 2nd Inversion
+            pass
+        elif "maj7_no_fifth" in chord:
+            # Major 7 no 5th
+            pass
+        elif "maj7_no_fifth_1st_inv" in chord:
+            # Major 7 no 5th 1st Inversion
+            pass
+        elif "maj7_no_fifth_2nd_inv" in chord:
+            # Major 7 no 5th 2nd Inversion
+            pass
+        elif "mush" in chord:
+            # Mush
+            pass
+        elif "maj7_add2" in chord:
+            # Major 7/9 no third no fifth
+            pass
+        elif "maj7_add2_1st_inv" in chord:
+            # Major 7/9 no third no fifth 1st inversion
+            pass
+        elif "maj7_add2_2nd_inv" in chord:
+            # Major 7/9 no third no fifth 2nd inversion
+            pass
+        elif "dim_maj7_no_fifth" in chord:
+            # Diminished major 7 with no fifth
+            pass
+        elif "dim_maj7_no_fifth_1st_inv" in chord:
+            # Diminished major 7 with no fifth 1st inversion
+            pass
+        elif "dim_maj7_no_fifth_2nd_inv" in chord:
+            # Diminished major 7 with no fifth 2nd inversion
+            pass
+        elif "sus_maj47" in chord:
+            # Suspended 4th with a major 7th
+            pass
+        elif "sus_maj47_1st_inv" in chord:
+            # Suspended 4th with a major 7th 1st inversion
+            pass
+        elif "sus_maj47_2nd_inv" in chord:
+            # Suspended 4th with a major 7th 2nd inversion
+            pass
+        elif "maj7_flat5" in chord:
+            # Major 7 flat 5
+            pass
+        elif "maj7_flat5_1st_inv" in chord:
+            # Major 7 flat 5 1st inversion
+            pass
+        elif "maj7_flat5_2nd_inv" in chord:
+            # Major 7 flat 5 2nd inversion
+            pass
+        elif "majmin" in chord:
+            # Major Minor
+            pass
+        elif "aug_maj7_3rd_inv_no_third" in chord:
+            # Major Minor 1st inversion
+            pass
+        elif "aug_maj7_no_third" in chord:
+            # Major Minor 2nd inversion
+            pass
+        elif "sus67" in chord:
+            # Suspened maj7 add 6
+            pass
+        elif "min_add2_no_fifth" in chord:
+            # Minor add 2
+            pass
+        elif "min_add2_no_fifth_1st_inv" in chord:
+            # Min add2 1st inversion
+            pass
+        elif "min7_no5" in chord:
+            # Min7 no 5th
+            pass
+        elif "sus56" in chord:
+            # Sus chord add 6
+            pass
+        elif "min7_no5_2nd_inv" in chord:
+            # Min7 no 5th 2nd inversion
+            pass
+        elif "major_flat5" in chord:
+            # Major flat 5
+            pass
+        elif "major_flat5_1st_inv" in chord:
+            # Major flat 5 1st inversion
+            pass
+        elif "major_flat5_2nd_inv" in chord:
+            # Major flat 5 2nd inversion
+            pass
+        elif "maj79" in chord:
+            # Major 7 add 9
+            pass
+        elif "maj79_1st_inv" in chord:
+            # Major 7 add 9 1st inversion
+            pass
+        elif "maj79_2nd_inv" in chord:
+            # Major 7 add 9 2nd inversion
+            pass
+        elif "maj_flat9" in chord:
+            # Major flat 9
+            pass
+        elif "maj_flat9_1st_inv" in chord:
+            # Major flat 9 1st inversion
+            pass
+        elif "maj_flat9_2nd_inv" in chord:
+            # Major flat 9 2nd inversion
+            pass
+        elif "maj_add9" in chord:
+            # Major add 9 no fifth
+            pass
+        elif "maj_add9_1st_inv" in chord:
+            # Major add 9 no fifth 1st inversion
+            pass
+        elif "maj_add9_2nd_inv" in chord:
+            # Major add 9 no fifth 2nd inversion
+            pass
+        elif "maj7_flat13" in chord:
+            # Major 7 flat 13 no third no fifth
+            pass
+        elif "sus4_flat9" in chord:
+            # Sus 4 flat 9 no third no fifth
+            pass
+        elif "sus49" in chord:
+            # Sus 4 add 9 no fifth
+            pass
+        elif "sus49_1st_inv" in chord:
+            # Sus 4 flat 9 no third no fifth 1st inversion
+            pass
+        elif "min_sus4" in chord:
+            # Sus 4 flat 9 no third no fifth 1st inversion
+            pass
+        elif "dominant_no_third_var" in chord:
+            # Dominant no third 
+            pass
+        elif "six_nine" in chord:
+            # Six nine chord
+            pass
+        elif "maj7_no_third_var" in chord:
+            # Major7 no third high fifth
+            pass
+        elif "maj7_no_third_var_1st_inv" in chord:
+            # Major7 no third high fifth 1st inversion
+            pass
+        elif "maj7_no_third_var_2nd_inv" in chord:
+            # Major7 no third high fifth 2nd inversion
+            pass
+        elif "sharp11_no_third" in chord:
+            # Sharp 11 no third
+            pass
+        elif "sharp11_no_third_1st_inv" in chord:
+            # Sharp 11 no third 1st inversion
+            pass
+        elif "sharp11_no_third_2nd_inv" in chord:
+            # Sharp 11 no third 2nd inversion
+            pass
+        elif "dominant_no_fifth_var" in chord:
+            # Dominant no fifth
+            pass
+        elif "dominant_no_fifth_var_1st_inv" in chord:
+            # Dominant no fifth 1st inversion
+            pass
+        elif "dominant_no_fifth_var_2nd_inv" in chord:
+            # Dominant no fifth 2nd inversion
+            pass
+        elif "maj_flat5_var" in chord:
+            # Major flat 5 
+            pass
+        elif "maj_flat5_var_1st_inv" in chord:
+            # Major flat 5 1st inversion
+            pass
+        elif "maj_flat5_var_2nd_inv" in chord:
+            # Major flat 5 2nd inversion
+            pass
+        elif "maj7_flat5_var" in chord:
+            # Major 7 flat 5 2nd inversion
+            pass
+        elif "maj7_flat5_var_1st_inv" in chord:
+            # Major 7 flat 5 1st inversion
+            pass
+        elif "maj7_flat5_var_2nd_inv" in chord:
+            # Major 7 flat 5 2nd inversion
+            pass
+        elif "min6_var" in chord:
+            # Minor 6
+            pass
+        elif "min6_var_2nd_inv" in chord:
+            # Minor 6 2nd inversion
+            pass
+        elif "min9_no5" in chord:
+            # Minor 9 no fifth
+            pass
+        elif "min9_no5_1st_inv" in chord:
+            # Minor 9 no fifth 1st inversion
+            pass
+        elif "min9_no5_2nd_inv" in chord:
+            # Minor 9 no fifth 2nd inversion
+            pass
+        else:
+            # Handle case where chord is not known
+            pass
 
     def recenter_frequency(self, message_heap: list[list], instance_index: int):
         """Used to recenter the base frequencies of instances which are no longer in use
