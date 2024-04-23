2208eab4896c463d23c23eed1aebf1bbd8bd87c9
Alex Wilson <awilson2244@gmail.com>
2024-04-23 00:16:39 -0400

    Added Chord sequence tree

---
+++ python/study/ChordSeqTree.py
@@ -0,0 +1,13 @@
+import itertools
+
+class ChordSeqTree:
+    
+    def __init__(self):
+        pass
+    
+    def generate_chord_sequences(chords: list[str], choices):
+        pass
+    
+
+if __name__ == "__main__":
+    pass
