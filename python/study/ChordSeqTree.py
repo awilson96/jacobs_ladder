4524a27f2e7178ce3b99cfee8d462314f636897e
Alex Wilson <awilson2244@gmail.com>
2024-04-23 00:49:31 -0400

    Created a generate chord sequences method

--- python/study/ChordSeqTree.py
+++ python/study/ChordSeqTree.py
@@ -5,9 +5,29 @@ class ChordSeqTree:
     def __init__(self):
         pass
     
-    def generate_chord_sequences(chords: list[str], choices):
-        pass
+    def generate_chord_sequences(self, chords: list, choices: int):
+        combinations = list(itertools.combinations(chords, choices))
+        print(f"With {len(chords)} possible chords and {choices} choices: \nThere are {len(combinations)} possible chord sequences\n")
+        
+        return combinations
     
 
 if __name__ == "__main__":
-    pass
+    cst = ChordSeqTree()
+    
+    chords = ["I Major 7", "I Dominant 7", "I Minor 7", 
+              "bII Major 7", "bII Dominant 7", 
+              "II Minor 7", "II Dominant 7", "II Minor 7 b5", 
+              "bIII Major 7", "bIII Diminished", 
+              "III Minor 7", "III Dominant 7", 
+              "IV Major 7", "IV Minor 7", "IV Minor 7 b5", 
+              "bV Minor 7 b5",
+              "V Dominant", "V Minor 7", 
+              "bVI Diminshed", "bVI Major 7", "bVI Dominant", 
+              "VI Minor 7", "VI Dominant 7",
+              "bVII Dominant", "bVII Minor 7",
+              "VII Diminished", "VII Minor 7 b5"]
+    
+    cst.generate_chord_sequences(chords=chords, choices=2)
+    cst.generate_chord_sequences(chords=chords, choices=3)
+    cst.generate_chord_sequences(chords=chords, choices=4)
