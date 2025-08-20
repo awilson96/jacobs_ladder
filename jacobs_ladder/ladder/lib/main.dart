5b73bf5b4e1fec44ac0124f1eed8abc55c8eca8e
Alex Wilson <awilson2244@gmail.com>
2025-08-20 19:36:27 -0400

    Made app constrained to minimum size

--- jacobs_ladder/ladder/lib/main.dart
+++ jacobs_ladder/ladder/lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:window_size/window_size.dart' as window_size;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    window_size.setWindowMinSize(const Size(400, 350));
+
+  }
   runApp(const LadderApp());
 }
