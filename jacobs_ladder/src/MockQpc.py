05e223902002608aa2342cadece2dd9336feff06
Alex Wilson <awilson2244@gmail.com>
2025-12-15 17:43:32 -0500

    Added MockQpc class

---
+++ jacobs_ladder/src/MockQpc.py
@@ -0,0 +1,12 @@
+class MockQpcUtils:
+    
+    def __init__(self):
+        pass
+    
+    def qpcGetTicks() -> int:
+        return 0
+    
+    def qpcGetFrequency() -> int:
+        return 10_000
+    
+    
